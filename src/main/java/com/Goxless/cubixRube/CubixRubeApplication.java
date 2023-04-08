054d3f9715470f7fe4ca975abdd2004a9d0aeb29
Goxless <Goxless@users.noreply.github.com>
2023-04-08 16:32:19 +0300

    initial commit

---
+++ src/main/java/com/Goxless/cubixRube/CubixRubeApplication.java
@@ -0,0 +1,12 @@
+package com.Goxless.cubixRube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubixRubeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CubixRubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Goxless/cubixRube/CubixRubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Goxless.cubixRube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubixRubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
